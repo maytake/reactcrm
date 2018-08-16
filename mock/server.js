3ede527e94785d030e35310def61b1bf67e55948
lq <1042689488@qq.com>
2018-08-16 18:00:51 +0800

    all

--- mock/server.js
+++ mock/server.js
@@ -1,7 +1,9 @@
-const Koa = require('koa');
-const app = new Koa();
-
-const router = require('koa-router')();
+var Koa = require('koa');
+var Router = require('koa-router');
+const cors = require('koa-cors');
+const bodyParser = require('koa-bodyparser')
+var app = new Koa();
+var router = new Router();
 
 
 var _memberData = require('./member/getmemberlist.js');
@@ -17,74 +19,24 @@ var _home6 = require('./home/getdepartmentlist.js')
 var normalData = require('./getSingleResult.js')
 
 
-router.post('/home/member/getmemberlist', function*(next) {
-    this.body = _memberData
-}).post('/home/member/getmemberinfo', function*(next) {
-    this.body = _memberData2
-}).post('/home/member/login', function*(next) {
-    this.body = _logo
-}).post('/home/index/getmenu', function*(next) {
-    this.body = _menu
-}).post('/home/department/getdepartmentlist', function*(next) {
-    this.body = _home
-}).post('/home/role/getallrolelist', function*(next) {
-    this.body = _home2
-}).post('/home/role/getrolelist', function*(next) {
-    this.body = _home3
-}).post('/home/role/getmembernodes', function*(next) {
-    this.body = _home4
-}).post('/home/role/getrolepriv', function*(next) {
-    this.body = _home5
-}).post('/home/department/getalldepartmentlist', function*(next) {
-    this.body = _home6
-}).post('/home/role/addrole', function*(next) {
-    this.body = normalData
-}).post('/home/department/adddepartment', function*(next) {
-    this.body = normalData
-}).post('/home/department/editdepartment', function*(next) {
-    this.body = normalData
-}).post('/home/department/deldepartment', function*(next) {
-    this.body = normalData
-}).post('/home/role/editrole', function*(next) {
-    this.body = normalData
-}).post('/home/role/setmemberextranode', function*(next) {
-    this.body = normalData
-}).post('/home/role/updaterolepriv', function*(next) {
-    this.body = normalData
-}).post('/home/role/delrole', function*(next) {
-    this.body = normalData
-}).post('/home/member/addmember', function*(next) {
-    this.body = normalData
-}).post('/home/member/setmemberstatus', function*(next) {
-    this.body = normalData
-}).post('/home/member/editmember', function*(next) {
-    this.body = normalData
-}).post('/home/member/logout', function*(next) {
-    this.body = normalData
-}).post('/home/member/batchsetmemberrole', function*(next) {
-    this.body = normalData
-}).post('/home/member/batchsetmemberdepartment', function*(next) {
-    this.body = normalData
-}).post('/home/member/batchsetmemberstatus', function*(next) {
-    this.body = normalData
-}).post('/home/member/resetpassword', function*(next) {
-    this.body = normalData
-}).post('/erp/area/addarea', function*(next) {
-    this.body = normalData
-}).post('/erp/area/editarea', function*(next) {
-    this.body = normalData
-}).post('/erp/store/authstore', function*(next) {
-    this.body = normalData
-}).post('/erp/store/editstore', function*(next) {
-    this.body = normalData
-}).post('/erp/store/addstore', function*(next) {
-    this.body = normalData
+router.post('/home/member/getmemberlist', async (ctx, next) => {
+    ctx.body = _memberData;
+    let {password, username} = ctx.request.body;
+    console.log(username);
+});
+router.get('/', async (ctx, next) => {
+    if(parseInt(ctx.status) === 404){
+        ctx.body = '404';
+    };
+    if(parseInt(ctx.status) === 500){
+        ctx.body = '500';
+    };
 });
 
 
-
 // 开始服务并生成路由
+app.use(cors());
+app.use(bodyParser());
 app.use(router.routes())
     .use(router.allowedMethods());
 app.listen(9090);
-console.log('app running on port 9090');
--- src/App.js
+++ src/App.js
@@ -4,41 +4,108 @@ import './App.css';
 import './a.scss';
 import NProgress from 'nprogress';
 import { Button } from 'antd';
+import 'ant-design-pro/dist/ant-design-pro.css';
 
+import Login from 'ant-design-pro/lib/Login';
+import { Alert, Checkbox } from 'antd';
+import GlobalFooter from 'ant-design-pro/lib/GlobalFooter';
+import { Icon } from 'antd';
+
+
+
+const { Tab, UserName, Password, Mobile, Captcha, Submit } = Login;
 class App extends Component {
   componentDidMount(){
-    var result = fetch('/home/member/getmemberlist', {
-      method: 'POST',
+    var dataVal={"username":"112107","password":"123456"}
+    
+    fetch('/home/member/getmemberlist', {
       credentials: 'include',
+      method: 'POST',
+      body:JSON.stringify(dataVal),
+      mode:'cors',
+      cache: 'default',
+      timeout: 20000,
       headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body:{}
+          'Content-Type': 'application/json;charset=UTF-8'
+      }
+ 
+      
     }).then(res => {
       
     });
     
   }
-  componentWillUpdate () {
-    NProgress.start()
+  state = {
+    notice: '',
+    type: 'tab2',
+    autoLogin: true,
   }
-
-  componentDidUpdate () {
-    NProgress.done()
+  onSubmit = (err, values) => {
+    console.log('value collected ->', { ...values, autoLogin: this.state.autoLogin });
+    if (this.state.type === 'tab1') {
+      this.setState({
+        notice: '',
+      }, () => {
+        if (!err && (values.username !== 'admin' || values.password !== '888888')) {
+          setTimeout(() => {
+            this.setState({
+              notice: 'The combination of username and password is incorrect!',
+            });
+          }, 500);
+        }
+      });
+    }
+  }
+  onTabChange = (key) => {
+    this.setState({
+      type: key,
+    });
+  }
+  changeAutoLogin = (e) => {
+    this.setState({
+      autoLogin: e.target.checked,
+    });
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="red"> wonderful</div>
-        <Button type="primary">Button</Button>
+      <div>
+      <Login
+        defaultActiveKey={this.state.type}
+        onTabChange={this.onTabChange}
+        onSubmit={this.onSubmit}
+      >
+        <Tab key="tab1" tab="Account">
+          {
+            this.state.notice &&
+            <Alert style={{ marginBottom: 24 }} message={this.state.notice} type="error" showIcon closable />
+          }
+          <UserName name="username" />
+          <Password name="password" />
+        </Tab>
+        <Tab key="tab2" tab="Mobile">
+          <Mobile name="mobile" />
+          <Captcha onGetCaptcha={() => console.log('Get captcha!')} name="captcha" />
+        </Tab>
+        <div>
+          <Checkbox checked={this.state.autoLogin} onChange={this.changeAutoLogin}>Keep me logged in</Checkbox>
+          <a style={{ float: 'right' }} href="">Forgot password</a>
+        </div>
+        <Submit>Login</Submit>
+        <div>
+          Other login methods
+          <span className="icon icon-alipay" />
+          <span className="icon icon-taobao" />
+          <span className="icon icon-weibo" />
+          <a style={{ float: 'right' }} href="">Register</a>
+        </div>
+      </Login>
+      
       </div>
     );
   }
+
+        
+   
 }
 
 export default App;
